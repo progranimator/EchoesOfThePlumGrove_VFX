01710a60f17aa4c9db401d07aad99b9bfe4b44e4
progranimator <progranimator@outlook.com>
2023-04-25 11:11:20 -0500

    Fish Thrash/Trail - First Pass v2
    - modified fish and water materials
    - added ribbon particle trails to fish trail
    - added aniamted , panning material to fish trail

---
+++ Assets/Scripts/Fish/Fish_PlayAnim.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish_PlayAnim : MonoBehaviour
+{
+    public Animator animator; // reference to the Animator component
+    public float animationSpeed = 1.0f; // public variable to adjust the speed of the animation
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); // get the Animator component on the same game object
+        //animator.Play("YourAnimationName"); // play the animation with the specified name
+    }
+
+    void Update()
+    {
+        animator.speed =
+            animationSpeed; // set the speed parameter of the Animator component to the value of the public variable
+    }
+}
+
